2f4a1fc738e1a985655f37320aaf84a0057578b0
tenzifrenzi <115435821+tenzifrenzi@users.noreply.github.com>
2022-10-11 14:28:13 -0700

    Add files via upload

---
+++ GeneralLibraries.h
@@ -0,0 +1,25 @@
+//
+// Created by Teni on 10/11/2022.
+//
+
+#ifndef HW2_GENERALLIBRARIES_H
+#define HW2_GENERALLIBRARIES_H
+    #include <strings.h>
+    #include <string>
+    #include <iostream>
+    #include <stdio.h>
+    #include <iomanip>
+    #include <cstdlib>
+    #include <sstream>
+    #include <ctime>
+    #include <exception>
+    #include <time.h>
+
+    // for Json::value
+    #include <json/json.h>
+    #include <json/reader.h>
+    #include <json/writer.h>
+    #include <json/value.h>
+
+    using namespace std;
+#endif //HW2_GENERALLIBRARIES_H
